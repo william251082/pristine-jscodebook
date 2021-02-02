89443c404004f8a2b7c1255ceb311d5f43d48cbd
William del Rosario <pristine.web.dev@gmail.com>
2021-02-02 11:14:14 +0100

    Issues with Multi-File Packages --doesn't work on package that have multiple files

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -31,7 +31,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('tiny-test-pkg');
+              const message = require('medium-test-pkg');
               console.log(message);
             `,
           };
