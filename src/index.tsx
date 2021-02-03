ab7af0d0d4e9506c8f3dd33cb4890b36b1b530a4
William del Rosario <pristine.web.dev@gmail.com>
2021-02-03 17:01:49 +0100

    Multiple Editors and Preview Windows

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,13 @@
 import ReactDOM from 'react-dom';
-import {useState} from "react";
-import CodeEditor from "./components/code-editor";
 import 'bulmaswatch/nuclear/bulmaswatch.min.css';
-import Preview from "./components/preview";
-import bundle from "./bundler";
+import CodeCell from "./components/code-cell";
 
 const App = () => {
-  const [code, setCode] = useState('');
-  const [input, setInput] = useState('');
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue="hi"
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>
-          Submit
-        </button>
-      </div>
-      <Preview code={code} />
+      <CodeCell/>
+      <CodeCell/>
+      <CodeCell/>
     </div>
   );
 };
