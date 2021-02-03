3aab8cd5c195e08d5628ffde494989b74ba2a2ea
William del Rosario <pristine.web.dev@gmail.com>
2021-02-03 17:58:18 +0100

    Constraining Vertical Resizes

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -9,6 +9,8 @@ interface ResizableProps {
 const Resizable : FC<ResizableProps> = ({direction, children}) => {
   return (
     <ResizableBox
+      minConstraints={[Infinity, 24]}
+      maxConstraints={[Infinity, window.innerHeight * 0.9]}
       height={300}
       width={Infinity}
       resizeHandles={['s']}
