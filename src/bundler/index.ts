c0ead0798a6d81f623b8a5f7900c2871b19fddc6
William del Rosario <pristine.web.dev@gmail.com>
2021-02-03 16:50:33 +0100

    Fixing a Few Warnings

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -1,11 +1,11 @@
 import * as esbuild from 'esbuild-wasm';
-import {fetchPlugin} from "../plugins/fetch-plugin";
-import {unpkgPathPlugin} from "../plugins/unpkg-path-plugin";
+import {fetchPlugin} from "./plugins/fetch-plugin";
+import {unpkgPathPlugin} from "./plugins/unpkg-path-plugin";
 
 let service: esbuild.Service;
-export default async (rawCode: string) => {
+const bundle = async (rawCode: string) => {
   if (!service) {
-    await esbuild.startService({
+    service = await esbuild.startService({
       worker: true,
       wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
     });
@@ -21,4 +21,6 @@ export default async (rawCode: string) => {
     }
   });
   return result.outputFiles[0].text;
-}
+};
+
+export default bundle;
