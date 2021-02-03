718c84b68130f9adb0ff7c078e3e42dc75d38fec
William del Rosario <pristine.web.dev@gmail.com>
2021-02-03 16:10:15 +0100

    Refactoring Out a Preview Component

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,11 @@ import {fetchPlugin} from "./plugins/fetch-plugin";
 import {unpkgPathPlugin} from "./plugins/unpkg-path-plugin";
 import CodeEditor from "./components/code-editor";
 import 'bulmaswatch/cyborg/bulmaswatch.min.css';
+import Preview from "./components/preview";
 
 const App = () => {
   const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code, setCode] = useState('');
   const [input, setInput] = useState('');
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -23,7 +24,7 @@ const App = () => {
     if (!ref.current) {
       return;
     }
-    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -34,42 +35,21 @@ const App = () => {
         global: 'window'
       }
     });
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
+    setCode(result.outputFiles[0].text);
   };
-  const html = `
-        <html>
-            <head></head>
-            <body>
-                <div id="root"></div>
-                <script>
-                    window.addEventListener('message', (event) => {
-                        try {
-                            eval(event.data);
-                        } catch(err) {
-                            const root = document.querySelector('#root')
-                            root.innerHTML = '<div style="color: red;"><h4>Runtime Error<h4/>' + err + '</div>';
-                            console.error(err);
-                        }
-                    }, false);
-                </script>
-            </body>
-        </html>
-    `;
+
   return (
     <div>
       <CodeEditor
         initialValue="hi"
         onChange={(value) => setInput(value)}
       />
-      <textarea value={input} onChange={(e) => {
-        setInput(e.target.value);
-      }}></textarea>
       <div>
         <button onClick={onClick}>
           Submit
         </button>
       </div>
-      <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html}/>
+      <Preview code={code} />
     </div>
   );
 };
