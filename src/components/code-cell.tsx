d07face8ba73d4088bee2694edf80df6ac322c1b
William del Rosario <pristine.web.dev@gmail.com>
2021-02-04 10:28:07 +0100

    Capturing Bundling Errors

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -7,11 +7,13 @@ import Resizable from "./resizable";
 const CodeCell = () => {
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
+  const [err, setErr] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(async() => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setErr(output.err);
     }, 1000);
     return () => {
       clearTimeout(timer)
