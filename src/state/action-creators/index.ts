9f849f46baf048506859993d0e0296d49c5a3dbc
William del Rosario <pristine.web.dev@gmail.com>
2021-02-04 13:41:54 +0100

    Action Creator Return Type Annotations

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,6 @@
+import {DeleteCellAction, InsertCellBeforeAction, MoveCellAction, UpdateCellAction} from "../actions";
+
+export const updateCell = (): UpdateCellAction => {};
+export const deleteCell = (): DeleteCellAction => {};
+export const moveCell = (): MoveCellAction => {};
+export const insertCellBefore = (): InsertCellBeforeAction => {};
