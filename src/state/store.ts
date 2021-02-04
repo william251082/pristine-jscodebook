a294838309e500ec29929a8fc290a3ce953363d3
William del Rosario <pristine.web.dev@gmail.com>
2021-02-04 15:38:02 +0100

    Small Gotcha with Immer

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,4 +1,9 @@
-import reducers from "./state/reducers";
+import reducers from "./reducers";
 import {applyMiddleware, createStore} from "redux";
+import thunk from "redux-thunk";
 
-export const store = createStore(reducers, {}, applyMiddleware());
+export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+// for ts, state should be returned in immer
+// const state = store.getState();
+// state.cells.data;
