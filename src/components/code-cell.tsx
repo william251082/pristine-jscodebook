58c41e928e066a298f0a368f7b6cb04ff926735a
William del Rosario <pristine.web.dev@gmail.com>
2021-02-05 11:19:43 +0100

    React Wire Up

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,30 +1,27 @@
 import {FC, useEffect, useState} from "react";
 import CodeEditor from "./code-editor";
 import Preview from "./preview";
-import bundle from "../bundler";
 import Resizable from "./resizable";
 import {Cell} from "../state";
 import {useActions} from "../hooks/use-actions";
+import {useTypedSelector} from "../hooks/use-typed-selector";
 
 interface CodeCellProps {
   cell: Cell
 }
 
 const CodeCell: FC<CodeCellProps> = ({cell}) => {
-  const [code, setCode] = useState('');
-  const [err, setErr] = useState('');
-  const { updateCell } = useActions();
-
+  const { updateCell, createBundle } = useActions();
+  const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+  console.log(bundle);
   useEffect(() => {
     const timer = setTimeout(async() => {
-      const output = await bundle(cell.content);
-      setCode(output.code);
-      setErr(output.err);
-    }, 1000);
+      createBundle(cell.id, cell.content)
+    }, 750);
     return () => {
       clearTimeout(timer)
     };
-  }, [cell.content]);
+  }, [cell.id, cell.content]);
 
   return (
     <Resizable direction="vertical">
@@ -41,7 +38,7 @@ const CodeCell: FC<CodeCellProps> = ({cell}) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        <Preview code={code} err={err} />
+        {/*<Preview code={code} err={err} />*/}
       </div>
     </Resizable>
 
