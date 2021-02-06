06c78fc43acad056f2585eb980fd8379a94b76d8
William del Rosario <pristine.web.dev@gmail.com>
2021-02-06 22:56:08 +0100

    Error Handling Solution

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -6,7 +6,19 @@ export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')
-  .action((filename = 'notebook.js', options: {port: string}) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(parseInt(options.port), path.basename(filename), dir);
+  .action(async(filename = 'notebook.js', options: {port: string}) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(parseInt(options.port), path.basename(filename), dir);
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit file.`
+      )
+    } catch (err) {
+      if (err.code === 'EADDRINUSE') {
+        console.error("Port is in use. Try running o n different port.")
+      } else {
+        console.log("Here's the problem", err.message);
+      }
+      process.exit(1);
+    }
   });
