72edd94ae4a49412f18bba974a378f06913975b8
William del Rosario <pristine.web.dev@gmail.com>
2021-02-06 23:07:24 +0100

    Connecting the Proxy

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import {createProxyMiddleware} from "http-proxy-middleware";
 
 export const serve = (port: number, filename: string, dir: string) => {
-    const app = express();
+  const app = express();
 
-    return new Promise<void>((resolve, reject) => {
-        app.listen(port, resolve).on('error', reject);
-    });
+  app.use(createProxyMiddleware({
+      target: 'http://localhost:3000',
+      ws: true,
+      logLevel: 'silent'
+    })
+  );
+
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on('error', reject);
+  });
 };
